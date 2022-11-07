7fdc23e2fa4382378912e827b95b6da792ad3567
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-11-07 10:29:27 +0100

    made the if statment more releiebel

---
+++ Template/src/ch/tbz/Athree.java
@@ -0,0 +1,22 @@
+package ch.tbz;
+import java.util.Scanner;
+
+public class Athree {
+    public static void main() {
+        Scanner scan = new Scanner(System.in);
+        int inputA;
+        int inputB;
+        int inputC;
+
+        //inputs
+        System.out.println("Enter the number A");
+        inputA = scan.nextInt();
+
+        System.out.println("Enter the number B");
+        inputB = scan.nextInt();
+
+        System.out.println("Enter the number C");
+        inputC = scan.nextInt();
+
+    }
+}
---
+++ Template/src/ch/tbz/suptraction.java
@@ -0,0 +1,4 @@
+package ch.tbz;
+
+public class suptraction {
+}
